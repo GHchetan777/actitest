fa9a0af996eef2c0c6a8ed457bae0313fde02ffd
chetan <chetan@DESKTOP-E8129AI>
2020-01-24 19:36:18 +0530

    Page factory Addition

--- pom class/src/pack/ActiTimeHomePage.java
+++ pom class/src/pack/ActiTimeHomePage.java
@@ -3,17 +3,24 @@ package pack;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class ActiTimeHomePage {
 	
+	@FindBy(xpath="//img[@src='/img/default/top_nav/default-logo.png?hash=274618146']")
 	private WebElement header;
+	
+	@FindBy(xpath="//*[@class=\"submit_tt myTT with-navigation  ext-chrome\"]")
 	private WebElement logout;
+	
 	WebDriver driver;
 	
 	public ActiTimeHomePage(WebDriver driver) {
 		this.driver = driver;
-		header = driver.findElement(By.xpath("//img[@src='/img/default/top_nav/default-logo.png?hash=274618146']"));
-		logout = driver.findElement(By.xpath("//*[@id=\"logoutLink\"]"));
+////		header = driver.findElement(By.xpath("//img[@src='/img/default/top_nav/default-logo.png?hash=274618146']"));
+////		logout = driver.findElement(By.xpath("//*[@id=\"logoutLink\"]"));
+		PageFactory.initElements(driver, this);
 		
 	}
 	
--- pom class/src/pack/ActitimeLoginPage.java
+++ pom class/src/pack/ActitimeLoginPage.java
@@ -3,22 +3,31 @@ package pack;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 
 
 public class ActitimeLoginPage {
 	//Declaration
+	@FindBy(xpath="//input[@name='username']")
 	private WebElement un;
+	
+	@FindBy(xpath="//input[@name='pwd']")
 	private WebElement pwd;
+	
+	@FindBy(xpath="//div[text()='Login ']")
 	private WebElement login;
 	WebDriver driver;
 	
 	public ActitimeLoginPage(WebDriver driver) {
 		// Initialization 
 		this.driver = driver;
-		un = driver.findElement(By.xpath("//input[@name='username']"));
-		pwd = driver.findElement(By.xpath("//input[@name='pwd']"));
-		login = driver.findElement(By.xpath("//div[text()='Login ']"));
+//		un = driver.findElement(By.xpath("//input[@name='username']"));
+//		pwd = driver.findElement(By.xpath("//input[@name='pwd']"));
+//		login = driver.findElement(By.xpath("//div[text()='Login ']"));
+		
+		PageFactory.initElements(driver, this);
 	
 	}
 	
